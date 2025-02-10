25a9a694466afd64c6e1f6a3134977617d6ea734
therappha <therappha@gmail.com>
2025-02-10 20:04:37 +0000

    changes

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,12 @@
 /*   By: rafaelfe <rafaelfe@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 17:42:20 by rafaelfe          #+#    #+#             */
-/*   Updated: 2025/01/28 22:47:21 by rafaelfe         ###   ########.fr       */
+/*   Updated: 2025/02/10 20:03:50 by rafaelfe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
+
 
 int	main(int ac, char **av)
 {
@@ -26,7 +27,7 @@ int	main(int ac, char **av)
 	int screen_y;
 
 	fdf->mlx_ptr = mlx_init();
-	fdf->win_ptr = mlx_new_window(fdf->mlx_ptr, 1360, 760, "FdF");
+	fdf->win_ptr = mlx_new_window(fdf->mlx_ptr, 1900, 1000, "FdF");
 	mlx_get_screen_size(fdf ->mlx_ptr, (&fdf ->screen_x), (&fdf-> screen_y));
 	drawmap(&fdf, &lines, atoi(av[2]));
 	mlx_hook((fdf) -> win_ptr, DestroyNotify, 0L, free_displays, &fdf);
