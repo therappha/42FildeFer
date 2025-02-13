4d795f7ce948c312e0e66855e5d974205fbb9ca1
Your Name <therappha@gmail.com>
2025-02-13 22:17:30 +0000

    added freemap function commented

--- srcs/free_displays.c
+++ srcs/free_displays.c
@@ -6,7 +6,7 @@
 /*   By: rafaelfe <rafaelfe@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 17:41:42 by rafaelfe          #+#    #+#             */
-/*   Updated: 2025/02/12 20:53:31 by rafaelfe         ###   ########.fr       */
+/*   Updated: 2025/02/13 22:10:39 by rafaelfe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int	free_displays(t_fdf *fdf)
 	mlx_destroy_window((fdf) -> mlx_ptr, (fdf) -> win_ptr );
 	mlx_destroy_display((fdf) -> mlx_ptr);
 	free((fdf) -> mlx_ptr);
+	ft_freemap(&(fdf->map));
 	ft_printf("Closing Application!");
 	exit(1);
 	return (0);
