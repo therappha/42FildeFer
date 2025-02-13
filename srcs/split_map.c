a4c99f86d64739660620d0e32d2ac38b01820872
Your Name <therappha@gmail.com>
2025-02-13 22:16:13 +0000

    added map parsing, not tested

--- srcs/split_map.c
+++ srcs/split_map.c
@@ -6,17 +6,35 @@
 /*   By: rafaelfe <rafaelfe@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 17:21:43 by rafaelfe          #+#    #+#             */
-/*   Updated: 2025/02/13 17:26:22 by rafaelfe         ###   ########.fr       */
+/*   Updated: 2025/02/13 21:35:21 by rafaelfe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 
-/*t_point *ft_split_points(char *str)
+
+
+t_map	**read_map(char *str)
 {
-	int	count_points;
+	int			fd;
+	char		*line;
+	t_map		**map;
+	t_map		*node;
+	int			line_count;
 
-	count_points =
-
-}*/
-
+	map = NULL;
+	line_count = 0;
+	fd = open(str, O_RDONLY);
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (line == NULL)
+			break;
+		node = ft_newmap(line, line_count);
+		if (!node)
+			return (free(line), NULL);
+		free(line);
+		ft_mapaddback(&map, node);
+		line_count ++;
+	}
+}
